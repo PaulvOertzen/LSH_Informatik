e572ea60842d2237ed8bb87e6e55052494258748
tobi <luchnertobias@protonmail.com>
2024-10-01 19:27:00 +0200

    One possible solution for setText + documentation

--- InformatikWettbewerb/Aufgabe1/src/setText.java
+++ InformatikWettbewerb/Aufgabe1/src/setText.java
@@ -1,3 +1,17 @@
-public static void setText(String text) {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class setText
+{
+    public static void main (String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new FileReader("")); // Directory where the .txt file is stored in
+        String input;
+
+        while ((input = reder.readLine()) != null)
+        {
+            System.out.println(input);
+        } reader.close();
+    }
 }

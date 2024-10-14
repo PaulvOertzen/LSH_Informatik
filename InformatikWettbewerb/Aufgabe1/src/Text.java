ea78a5f1b7057d46e01c930bca729048818682ba
Babsi224 <mader_barbara@lsh-marquartstein.de>
2024-10-14 14:01:18 +0200

    Update Text.java

--- InformatikWettbewerb/Aufgabe1/src/Text.java
+++ InformatikWettbewerb/Aufgabe1/src/Text.java
@@ -1,13 +1,4 @@
 public class Text extends Control {
-    String text;
-public int getChar(){
-    String str = text;
-    String text = str.replaceAll("[^a-zA-Z]", "");
-        // Use regular expression to match all non-alphabetic characters and replace with empty string
-    char[] charArray = new char[str.length()];
-    str.getChars(0, str.length(), charArray, 0);
-    return charArray.length;
-}
     public void edit() {
 
     }

6b53de5c8432f92725f94bbb42508f18cc4bf89c
SirAlexus1811 <gummi.hubi@gmail.com>
2024-11-09 18:34:29 +0100

    Little Changes to newly added Classes (Comments)

--- InformatikWettbewerb/Aufgabe3/Wandertag/src/Point.java
+++ InformatikWettbewerb/Aufgabe3/Wandertag/src/Point.java
@@ -1,20 +1,25 @@
+//Point Class that is needed by Kmeans
 public class Point {
     private double x;
     private double y;
 
+    //Constructor, that sets X,Y Instance Vars to given Parameters
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
+    //Returns X Position
     public double getX() {
         return x;
     }
 
+    //Returns Y Position
     public double getY() {
         return y;
     }
 
+    //Returns distance to another Point
     public double distance(Point other) {
         return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
     }

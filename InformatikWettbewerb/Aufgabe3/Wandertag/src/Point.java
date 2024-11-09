2a19cf4ad1326c9a8fc1e8702cc3ebc9ffe65672
Babsi224 <mader_barbara@lsh-marquartstein.de>
2024-11-09 13:49:10 +0100

    Create Point.java

---
+++ InformatikWettbewerb/Aufgabe3/Wandertag/src/Point.java
@@ -0,0 +1,21 @@
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double distance(Point other) {
+        return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
+    }
+}

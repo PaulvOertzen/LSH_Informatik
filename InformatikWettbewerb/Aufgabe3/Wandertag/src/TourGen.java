75f32397ed482c8227147209a69af8d630b0da17
Babsi224 <mader_barbara@lsh-marquartstein.de>
2024-11-11 14:34:55 +0100

    Update TourGen.java

--- InformatikWettbewerb/Aufgabe3/Wandertag/src/TourGen.java
+++ InformatikWettbewerb/Aufgabe3/Wandertag/src/TourGen.java
@@ -1,4 +1,13 @@
 //Class that utilizes Kmeans to generate the 3 Tours/Products
-public class TourGen {
-    
+import java.io*;
+import java.util.*;
+
+public class TourGen extends Kmeans {
+public static void main (String[] args) {
+int max = 70, min = 20;
+for (int i = 0; i < 3; i++)
+    {
+System.out.println((max + min) \ 2);
+    }
+}    
 }

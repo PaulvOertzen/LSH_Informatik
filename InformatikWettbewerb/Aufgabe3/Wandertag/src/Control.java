b782d345f6c26633f2a29482a5b81df83bcdcad1
SirAlexus1811 <gummi.hubi@gmail.com>
2024-11-16 20:09:15 +0100

    Added Counting and Matching Datasets to tours

--- InformatikWettbewerb/Aufgabe3/Wandertag/src/Control.java
+++ InformatikWettbewerb/Aufgabe3/Wandertag/src/Control.java
@@ -8,10 +8,14 @@ public class Control {
     private Reader reader = null; //Instance of Reader Class
     private TourGen tourgen = null;
     public double tours[]; //Finished Tours given by Tourgen
+    private List<int[]> foundParticipants = null;
+    private int countParticipants; // = foundParticipants.length()
 
     //Constructer Creates new Instance of Reader Class
     public Control() {
         this.dataPoints = new ArrayList<>();
+        this.foundParticipants = new ArrayList<>();
+        this.countParticipants = 0;
     }
 
     //Runs test of Reader Class
@@ -41,15 +45,42 @@ public class Control {
 
         System.out.println("Algorythm Done!");
         
-        this.showTours();
+        //Search for matching Datasets
+        this.findPeople();
+
+        this.showData();
+    }
+
+    //Searches the entries which will participate in one of these tours
+    public void findPeople() {
+        //Diese Funktion macht: Nimmt einen Dataset, überprüft eine ausgerechnete Tour zischen 
+        //min und max liegt wenn ja dann speicher er diese in die BufferArray welche dann zu der 
+        //Liste hinzugefügt wird bei dem eintrag 1 für die Tour 1
+        for (int i = 0; i < tours.length; i++) {
+            for (int j = 0; j < distances.length; j++) {
+                //If tours is in betwenn min and max of j. entry of distances
+                if (this.tours[i] >= this.distances[j][0] && this.tours[i] <= this.distances[j][1]) {
+                    this.foundParticipants.add(new int[]{i, j}); //Save Indeces of Tour and dataset in Distances
+                } 
+            }
+        }
+
     }
 
     //Shows calculated Tours
-    private void showTours() {
+    private void showData() {
         System.out.println("Showing calculated Tours... ");
         for (int i = 0; i < tours.length; i++) {
             System.out.println("Tour Number " + i + ": " + tours[i]);
         }
+        System.out.println("Showing Matching Datasets to Tours...");
+        for (int[] participant : foundParticipants) {
+            //Get saved indeces to print them
+            int tour = participant[0];
+            int dataset = participant[1];
+            System.out.println("Tour mit " + this.tours[tour] + "m liegt im Bereich von " + this.distances[dataset][0] + "m bis " + this.distances[dataset][1] + "m.");
+        }
+        System.out.println("Insgesamt Teilnehmer: " + foundParticipants.size() + " von " + distances.length);
         System.out.println("Done Showing Data.");
     }
 
